8c83b751b19167c570880c123f62f36e71dbf972
xialiao <xialiao@illinois.edu>
2017-07-31 01:01:28 +0000

    configure ngx-bootstrap

--- systemjs.config.server.js
+++ systemjs.config.server.js
@@ -26,7 +26,8 @@
       // other libraries
       'd3': 'node_modules/d3/d3.min.js',
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'ngx-bootstrap': 'npm:ngx-bootstrap/bundles/ngx-bootstrap.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -43,7 +44,10 @@
       },
       d3: {
         defaultExtension: 'js'
-      }
+      },
+      'ngx-bootstrap': { 
+        defaultExtension: 'js'
+      },
     }
   });
 })(this);
